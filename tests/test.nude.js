b4f1cc2682f03c7984cfcc2368bede5aa5874996
goddamnbugs <steve@goddamnbugs.com>
2011-05-06 22:56:15 -0400

    adjusted test slightly

--- tests/test.nude.js
+++ tests/test.nude.js
@@ -4,11 +4,18 @@
 
 var nude = new (require('../lib/node-nude')).nude();
 
-var filename = __dirname + '/images/4.jpg';
-nude.isnude(filename, function(result) {
-  if (result) {
-    console.log(filename + " : Nude!!");
-  } else {
-    console.log(filename + " : No nude!!");
-  }
-});
+function scan(filename) {
+  nude.isnude(filename, function(result) {
+    if (result) {
+      console.log(filename + " : Nude!!");
+    } else {
+      console.log(filename + " : No nude!!");
+    }
+  });
+}
+
+scan(__dirname + '/images/4.jpg');
+// uncomment this once the speed issue has been addressed...right now it isn't pretty
+//for (var i = 1; i < 5; i++) {
+//  scan(__dirname + '/images/' + i +'.jpg');
+//}
